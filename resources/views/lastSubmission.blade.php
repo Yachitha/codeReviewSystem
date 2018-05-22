e1734f974447960d75ac3c45e5863e6689a7980b
Vishmantha7 <vishmantha7.com>
2018-05-22 14:39:02 +0530

    final

--- resources/views/lastSubmission.blade.php
+++ resources/views/lastSubmission.blade.php
@@ -11,33 +11,23 @@
         <div class="card" style="width: 100%;">
         <div class="card-header">
             <div class="form-group">
-            <label for="exampleFormControlSelect1">Select the language</label>
-            <select class="form-control" id="langSelect" onclick="langSelect()">
-              <option value="javascript">Javascript</option>
-              <option value="java">Java</option>
-              <option value="c++">C++</option>
-              <option value="python">Python</option>
-              <option value="c">C</option>
-              <option value="php">PHP</option>
-              <option value="html">HTML</option>
-            </select>
+           
           </div>
-          <p id="demo"></p>
+       
+
           </div>
           <div class="card-body">
-             <pre id="editor" style="height: 350px;">
-//copy and paste your code here
-            </pre>  
+             
+              <textarea class="form-control" id="result" rows="8" style="padding-bottom: 20px" ></textarea>
           </div>
           <div style="padding-top:10px; padding-left: 20px; padding-right: 20px; background-color:#000; opacity:0.95 ">
-          <button type="submit" class="btn btn-primary mb-2" style="padding-top:10px; padding-right: 20px; padding-left: 20px" onclick="submit()" >Submit</button>
           </div>
           <div class="form-group" style="padding-right: 20px; padding-left: 20px; padding-bottom: 20px">
             <label for="exampleFormControlTextarea1"  style="width: 80%" >Result</label>
             <!-- <p id="result"></p> -->
             <textarea class="form-control" id="result" rows="3" style="padding-bottom: 20px" ></textarea>
             <div style="padding-top: 20px">
-             <a class="btn btn-primary mb-2" style="padding-top:10px; padding-right: 20px; padding-left: 20px"  href="/lastSubmission" >Last Submission</a>
+             <a class="btn btn-primary mb-2" style="padding-top:10px; padding-right: 20px; padding-left: 20px"  href="/home" >Editor</a>
              </div>
           </div>
         </div>
